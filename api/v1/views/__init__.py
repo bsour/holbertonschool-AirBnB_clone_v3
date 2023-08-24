5a316a3415c65c8ea34385f3106dadfaaf12555f
belindaHBTN <6372@holbertonstudents.com>
2023-08-24 04:09:42 -0700

    1. added url prefix for app_views in app.py 2. created __init__.py in views folder with variable app_views 3. created index.py with status route on app_views

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""__init__.py inside views folder of v1 package"""
+from flask import Blueprint
+from api.v1.views.index import *
+
+app_views = Blueprint("app_views", __name__)
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""blueprint index"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status", method=['GET'])
+def status():
+    """return status JSON"""
+    return jsonify({"status": "OK"})
