38f0801224b5565823969c084d78ab31b1114f81
belindaHBTN <6372@holbertonstudents.com>
2023-08-24 05:27:51 -0700

    updated Task4 1. comment in import from index, but changed the order 2. modified typo method-> methods in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status", method=['GET'])
+@app_views.route("/status", methods=['GET'])
 def status():
     """return status JSON"""
     return jsonify({"status": "OK"})
