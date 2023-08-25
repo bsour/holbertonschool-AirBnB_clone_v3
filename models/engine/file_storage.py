2194ee33ebf699632f93df48564f8d2605f5c95c
belindaHBTN <6372@holbertonstudents.com>
2023-08-24 18:31:42 -0700

    Update DBStorage and FileStorage with get method & count method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID"""
+        key = "{}.{}".format(cls, id)
+        if key in self.__objects.keys():
+            return self.__objects[key]
+        return
+
+    def count(self, cls=None):
+        """Returns number of objects in storage matching the given class."""
+        obj_dict = self.all(cls)
+        return len(obj_dict)
