0d6c7fd22d1188af84a5eb7e4f92d556eeedc80d
belindaHBTN <6372@holbertonstudents.com>
2023-08-24 20:16:18 -0700

    created an endpoint that retrieved the number of each obj by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,20 @@
 """blueprint index"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'])
 def status():
     """return status JSON"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=['GET'])
+def stats():
+    """return the number of instances for each class"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
