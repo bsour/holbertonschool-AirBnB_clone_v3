41ee0890ebe743f1675a32e75ef1dc2f3d6272d4
belindaHBTN <6372@holbertonstudents.com>
2023-08-24 20:22:59 -0700

    added an errorhandler for error msg in json format

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Main application file"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def teardown(exc):
     """Close session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return a JSON-formatted 404 status"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
