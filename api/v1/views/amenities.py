036b608a306fed4d9f62a7017105b2c1cbc377f5
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-25 22:21:36 -0700

    same fix as states.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.amenity import Amenity
+from json import JSONDecodeError
 
 
 @app_views.route("/amenities", methods=["GET"], strict_slashes=False)
@@ -41,11 +42,14 @@ def delete_amenity(amenity_id):
 
 @app_views.route("/amenities", methods=["POST"], strict_slashes=False)
 def post_amenity():
-    """Creates a Amenity"""
+    """Creates an Amenity"""
     try:
         new_amenity = request.get_json()
-    except Exception as e:
-        return jsonify({"error": "Not a JSON"}), 400
+    except JSONDecodeError as e:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    if not new_amenity:
+        return jsonify({"error": "No JSON data provided"}), 400
 
     if "name" not in new_amenity:
         return jsonify({"error": "Missing name"}), 400
@@ -57,15 +61,18 @@ def post_amenity():
 
 @app_views.route("/amenities/<amenity_id>", methods=["PUT"])
 def put_amenity(amenity_id):
-    """Updates a Amenity object"""
+    """Updates an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
-        abort(404)
+        return jsonify({"error": "Amenity not found"}), 404
 
     try:
         update_amenity = request.get_json()
-    except Exception as e:
-        return jsonify({"error": "Not a JSON"}), 400
+    except JSONDecodeError as e:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    if not update_amenity:
+        return jsonify({"error": "No JSON data provided"}), 400
 
     for key, value in update_amenity.items():
         if key not in ["id", "created_at", "updated_at"]:
