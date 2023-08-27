5b510d0f38d32a73a8266c2cab7b017c589cde11
belindaHBTN <6372@holbertonstudents.com>
2023-08-26 22:45:18 -0700

    13. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Main application file"""
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -29,9 +31,3 @@ if __name__ == "__main__":
     if app_port is None:
         app_port = 5000
     app.run(host=app_host, port=app_port)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """Return a JSON-formatted 404 status"""
-    return make_response(jsonify({"error": "Not found"}), 404)
